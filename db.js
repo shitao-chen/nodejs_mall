d1cb72dd6b8577341fb7c1b487b69a1ebe291a88
shitao-chen <9164711607@qq.com>
2018-10-17 10:27:01 +0800

    搭建项目的框架

---
+++ db.js
@@ -0,0 +1,13 @@
+
+let mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/product-manager",{useNewUrlParser:true});
+
+let connection = mongoose.connection;
+
+connection.on("error", err => {
+    console.log("数据库连接失败:" + err.toString())
+});
+connection.once("open", () => {
+    console.log("连接成功")
+});
