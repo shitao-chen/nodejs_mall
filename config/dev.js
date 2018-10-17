2cd1118733edeb1cfc52e9f4fe97ed6f4724b761
shitao-chen <9164711607@qq.com>
2018-10-17 17:51:26 +0800

    修改一些小问题

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 //开发环境
 module.exports = {
-    PORT:8000,
-    DB:"product-manager-test",
+    PORT: 8000,
+    DB: "product-manager-test",
     TOKEN_EXPIRE: 1000 * 60 * 60,
     TOKEN_KEY: "DEV"
 };
